11c957167a5e5ef1c559a2a4e7ab3db0ee80e18d
David Guerrero <davidguerrero@Davids-MacBook-Pro.local>
2024-03-13 16:14:43 -0500

    Explanation of pure and impure function.

---
+++ 03_pure_impure_function.py
@@ -0,0 +1,26 @@
+# PURE FUNCTION => These functions use only their parameters to generate a result, 
+# not affecting the state of the program. Furthermore, they always produce the same
+# result with the same arguments. 
+def sum(num_1, num_2):
+    return num_1 + num_2
+
+
+print(f"PURE FUNCTION. \n2 + 4 = {sum(2, 4)}.")
+print(f"2 + 4 = {sum(2, 4)}.")  # Same result. 
+print(f"2 + 4 = {sum(2, 4)}. \n")
+
+
+# These functions use their parameters and the environment variables to generate a 
+# response. Furthermore, they produce different results with the same arguments, 
+# making it more difficult to predict their behavior, due to this, it’s recommended
+# not to use impure functions in the possible (this is a good practice).
+num_1 = [2]
+
+def sum(num_2):
+    num_1[0] += num_2
+    return num_1[0]
+
+
+print(f"IMPURE FUNCTION. \n2 + 4 = {sum(4)}.")
+print(f"2 + 4 = {sum(4)}.")  # Different results. 
+print(f"2 + 4 = {sum(4)}.")
